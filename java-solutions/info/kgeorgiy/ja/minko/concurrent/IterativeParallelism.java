1ebb447805ca08206938ed4e1726fba4858100c5
Elvira Minko <334960@niuitmo.ru>
2023-04-14 13:41:33 +0300

    hw8

--- java-solutions/info/kgeorgiy/ja/minko/concurrent/IterativeParallelism.java
+++ java-solutions/info/kgeorgiy/ja/minko/concurrent/IterativeParallelism.java
@@ -1,6 +1,7 @@
 package info.kgeorgiy.ja.minko.concurrent;
 
 import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,53 +17,80 @@ import java.util.function.Predicate;
  */
 
 public class IterativeParallelism implements ScalarIP {
+    private final ParallelMapper parallelMapper;
+
+    /**
+     * Default constructor
+     */
+    public IterativeParallelism() {
+        parallelMapper = null;
+    }
+
+    /**
+     * Constructor from {@link ParallelMapper}
+     */
+    public IterativeParallelism(ParallelMapper parallelMapper) {
+        this.parallelMapper = parallelMapper;
+    }
 
     private <T> List<List<? extends T>> splitValues(int threads, List<? extends T> values) {
         List<List<? extends T>> returnValue = new ArrayList<>(threads);
         int block = values.size() / threads;
         int tail = values.size() % threads;
-        int[] blocks = new int[threads];
+        threads = Math.min(threads, values.size());
+        int start = 0;
+        int end = block;
         for (int i = 0; i < threads; i++) {
-            blocks[i] = block;
             if (tail != 0) {
-                blocks[i]++;
                 tail--;
+                end++;
             }
-        }
-        int prefixSize = 0;
-        for (int i = 0; i < threads; i++) {
-            if (blocks[i] > 0) {
-                returnValue.add(values.subList(prefixSize, prefixSize + blocks[i]));
-            }
-            prefixSize += blocks[i];
+            returnValue.add(values.subList(start, end));
+            start = end;
+            end += block;
         }
         return returnValue;
     }
 
+    private void finishThreadsWork(List<Thread> threadList, boolean isInterrupt, InterruptedException exception) {
+        for (int i = 0; i < threadList.size(); i++) {
+            try {
+                threadList.get(i).join();
+            } catch (InterruptedException e) {
+                if (isInterrupt) {
+                    for (int j = i; j < threadList.size(); j++) {
+                        threadList.get(j).interrupt();
+                    }
+                }
+                isInterrupt = false;
+                exception.addSuppressed(e);
+                finishThreadsWork(threadList.subList(i, threadList.size()), false, exception);
+            }
+        }
+    }
 
     private <T, U> List<U> function(int threads, List<? extends T> values, Function<List<? extends T>, U> function) throws InterruptedException {
         var splitValues = splitValues(threads, values);
-
-        List<U> result = new ArrayList<>(Collections.nCopies(threads, null));
-
-        List<Thread> threadList = new ArrayList<>(threads);
-        for (int i = 0; i < splitValues.size(); i++) {
-            int finalI = i;
-            Thread thread = new Thread(() ->
-                    result.set(finalI, function.apply(splitValues.get(finalI))));
-            threadList.add(thread);
-            thread.start();
-        }
-
-        for (Thread thread : threadList) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                for (Thread value : threadList) {
-                    value.interrupt();
-                }
-                throw new InterruptedException(e.getMessage());
+        List<U> result;
+        if (parallelMapper == null) {
+            result = new ArrayList<>(Collections.nCopies(splitValues.size(), null));
+            List<Thread> threadList = new ArrayList<>(threads);
+            for (int i = 0; i < splitValues.size(); i++) {
+                int finalI = i;
+                Thread thread = new Thread(() ->
+                        result.set(finalI, function.apply(splitValues.get(finalI))));
+                threadList.add(thread);
+                thread.start();
             }
+
+            boolean isInterrupt = true;
+            InterruptedException exception = new InterruptedException();
+            finishThreadsWork(threadList, isInterrupt, exception);
+            if (!isInterrupt) {
+                throw new InterruptedException("Interrupted exceptions: " + exception.getMessage());
+            }
+        } else {
+            result = parallelMapper.map(function, splitValues);
         }
         return result;
     }
---
+++ java-solutions/info/kgeorgiy/ja/minko/concurrent/ParallelMapperImpl.java
@@ -0,0 +1,94 @@
+package info.kgeorgiy.ja.minko.concurrent;
+
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * Implements an {@link ParallelMapper} interface
+ *
+ * @author Elvira Minko
+ */
+public class ParallelMapperImpl implements ParallelMapper {
+
+    private final List<Thread> threadList = new ArrayList<>();
+
+    private final Deque<Runnable> tasksQueue = new ArrayDeque<>();
+
+    public ParallelMapperImpl(int threads) {
+        for (int i = 0; i < threads; i++) {
+            threadList.add(new Thread(() -> {
+                try {
+                    while (!Thread.interrupted()) {
+                        Runnable task;
+                        synchronized (tasksQueue) {
+                            while (tasksQueue.isEmpty()) {
+                                tasksQueue.wait();
+                            }
+                            task = tasksQueue.remove();
+                        }
+                        task.run();
+                    }
+                } catch (InterruptedException e) {
+                    //
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+            }));
+            threadList.get(i).start();
+        }
+    }
+
+    private static class MapperList<T> {
+        List<T> list;
+        int unmappedElements;
+
+        public MapperList(int size) {
+            list = new ArrayList<>(Collections.nCopies(size, null));
+            this.unmappedElements = size;
+        }
+
+        public synchronized void set(int index, T element) {
+            list.set(index, element);
+            unmappedElements--;
+            this.notify();
+        }
+
+        public synchronized List<T> getList() throws InterruptedException {
+            while (unmappedElements > 0) {
+                this.wait();
+            }
+            return list;
+        }
+    }
+
+
+    @Override
+    public <T, R> List<R> map(Function<? super T, ? extends R> f, List<? extends T> args) throws InterruptedException {
+        MapperList<R> result = new MapperList<>(args.size());
+
+        for (int i = 0; i < args.size(); i++) {
+            synchronized (tasksQueue) {
+                final T element = args.get(i);
+                int finalI = i;
+                tasksQueue.add(() -> result.set(finalI, f.apply(element)));
+                tasksQueue.notifyAll();
+            }
+        }
+
+        return result.getList();
+    }
+
+    @Override
+    public void close() {
+        for (Thread thread : threadList) {
+            thread.interrupt();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                //
+            }
+        }
+    }
+}
