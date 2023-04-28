ccd2ce885c73ebbc0b04c4584c5cc15aa526e053
Elvira Minko <334960@niuitmo.ru>
2023-04-28 14:03:55 +0300

    hw9

---
+++ java-solutions/info/kgeorgiy/ja/minko/crawler/WebCrawler.java
@@ -0,0 +1,101 @@
+package info.kgeorgiy.ja.minko.crawler;
+
+import info.kgeorgiy.java.advanced.crawler.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.*;
+
+public class WebCrawler implements Crawler {
+
+    private final Downloader downloader;
+    private final ExecutorService downloadTreads;
+    private final ExecutorService extractorTreads;
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<Runnable>> hosts = new ConcurrentHashMap<>();
+
+    public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
+        this.downloader = downloader;
+        this.downloadTreads = Executors.newFixedThreadPool(downloaders);
+        this.extractorTreads = Executors.newFixedThreadPool(extractors);
+    }
+
+    @Override
+    public Result download(String url, int depth) {
+        Set<String> result = ConcurrentHashMap.newKeySet();
+        Set<String> visitedLinks = ConcurrentHashMap.newKeySet();
+        Set<String> newLayer = ConcurrentHashMap.newKeySet();
+        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
+        ConcurrentHashMap<String, IOException> exceptions = new ConcurrentHashMap<>();
+        Phaser phaser = new Phaser(1);
+
+        visitedLinks.add(url);
+        queue.add(url);
+        for (int i = depth; i >= 1; i--) {
+            for (String link : queue) {
+                downloadLinks(link, result, exceptions, visitedLinks, phaser, newLayer, i);
+            }
+            phaser.arriveAndAwaitAdvance();
+            queue.clear();
+            queue.addAll(newLayer);
+            newLayer.clear();
+
+        }
+        return new Result(new ArrayList<>(result), exceptions);
+    }
+
+    private void downloadLinks(String url, Set<String> result, ConcurrentHashMap<String, IOException> exceptions, Set<String> visitedLinks, Phaser phaser, Set<String> newLayer, int depth) {
+        try {
+            String host = URLUtils.getHost(url);
+            ConcurrentLinkedQueue<Runnable> hostWorker = hosts.computeIfAbsent(host, string -> new ConcurrentLinkedQueue<>());
+            phaser.register();
+            Runnable runnable = () -> {
+                try {
+                    Document document = downloader.download(url);
+                    result.add(url);
+                    if (depth != 1) {
+                        phaser.register();
+                        extractorTreads.submit(() -> {
+                            try {
+                                for (String link : document.extractLinks()) {
+                                    if (!visitedLinks.contains(link)) {
+                                        visitedLinks.add(link);
+                                        newLayer.add(link);
+                                    }
+                                }
+                            } catch (IOException e) {
+                                System.err.println("Exception in extracting links:" + e.getMessage());
+                            } finally {
+                                phaser.arriveAndDeregister();
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    exceptions.put(url, e);
+                } finally {
+                    phaser.arriveAndDeregister();
+                    Runnable task = hostWorker.poll();
+                    if (task != null) {
+                        downloadTreads.submit(task);
+                    }
+                }
+            };
+
+            try {
+                downloadTreads.submit(runnable);
+            } catch (RejectedExecutionException e) {
+                hostWorker.add(runnable);
+            }
+        } catch (MalformedURLException e) {
+            exceptions.put(url, e);
+        }
+    }
+
+
+    @Override
+    public void close() {
+        extractorTreads.shutdown();
+        downloadTreads.shutdown();
+    }
+}
