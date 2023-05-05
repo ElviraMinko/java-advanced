51f352c2fc7b20023d988722b6aa2dd3a7ce8da2
Elvira Minko <334960@niuitmo.ru>
2023-05-05 15:13:40 +0300

    hw10.1

--- java-solutions/info/kgeorgiy/ja/minko/hello/HelloUDPClient.java
+++ java-solutions/info/kgeorgiy/ja/minko/hello/HelloUDPClient.java
@@ -48,14 +48,13 @@ public class HelloUDPClient implements HelloClient {
 
         ExecutorService threadPool = Executors.newFixedThreadPool(threads);
         InetSocketAddress inetSocketAddress = new InetSocketAddress(host, port);
-        for (int i = 0; i < threads; i++) {
+        for (int i = 1; i < threads + 1; i++) {
             int finalI = i;
             threadPool.submit(() -> {
                 try (DatagramSocket datagramSocket = new DatagramSocket()) {
                     datagramSocket.setSoTimeout(239);
                     DatagramPacket datagramPacket = createPacket(datagramSocket, inetSocketAddress);
-
-                    for (int j = 0; j < requests; j++) {
+                    for (int j = 1; j < requests + 1; j++) {
                         String message = createMessage(prefix, finalI, j);
                         System.out.println("Sending:" + message);
                         while (true) {
