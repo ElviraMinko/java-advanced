8f3a62e9059833bf0800f0d1b1535f894cc4e76a
Elvira Minko <334960@niuitmo.ru>
2023-06-02 14:32:57 +0300

    hw12.3

--- java-solutions/info/kgeorgiy/ja/minko/hello/HelloUDPNonblockingClient.java
+++ java-solutions/info/kgeorgiy/ja/minko/hello/HelloUDPNonblockingClient.java
@@ -7,16 +7,16 @@ import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.*;
 
 public class HelloUDPNonblockingClient extends AbstractHelloUDPClient {
     @Override
     public void run(String host, int port, String prefix, int threads, int requests) {
         InetSocketAddress inetSocketAddress = new InetSocketAddress(host, port);
         ArrayList<Integer> list = new ArrayList<>(Collections.nCopies(threads, 1));
-        try (Selector selector = Selector.open()) {
+        Selector selector = null;
+        try {
+            selector = Selector.open();
             for (int i = 1; i <= threads; i++) {
                 DatagramChannel datagramChannel = DatagramChannel.open();
                 datagramChannel.configureBlocking(false);
@@ -29,7 +29,8 @@ public class HelloUDPNonblockingClient extends AbstractHelloUDPClient {
                 }
                 for (final Iterator<SelectionKey> i = selector.selectedKeys().iterator(); i.hasNext(); ) {
                     SelectionKey key = i.next();
-                    try (DatagramChannel channel = (DatagramChannel) key.channel()){
+                    DatagramChannel channel = (DatagramChannel) key.channel();
+                    try {
                         int threadId = (Integer) key.attachment() - 1;
                         String message = createMessage(prefix, threadId + 1, list.get(threadId));
                         if (key.isReadable()) {
@@ -54,10 +55,26 @@ public class HelloUDPNonblockingClient extends AbstractHelloUDPClient {
                 }
 
             }
-        } catch (IOException e) {
-            System.err.println("IOException handled");
+        } catch (Exception e) {
+            System.err.println("Exception handled");
+            if (selector != null) {
+                selector.keys().stream().map(SelectionKey::channel).forEach(selectableChannel -> {
+                    try {
+                        selectableChannel.close();
+                    } catch (IOException exception) {
+                        System.err.println("I/O exception occurs in closing channel");
+                    }
+                });
+            }
+        } finally {
+            if (selector != null) {
+                try {
+                    selector.close();
+                } catch (IOException e) {
+                    System.err.println("Can't close selector");
+                }
+            }
         }
-
     }
 
     private String createMessage(String prefix, int numberThread, int numberRequest) {
